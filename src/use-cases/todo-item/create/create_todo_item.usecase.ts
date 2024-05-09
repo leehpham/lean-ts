39c47e26470de4ee9d11a779f995e08a24031e0e
Hoang Pham <hoang.pham@enouvo.com>
2024-05-09 14:17:19 +0700

    add_repositories: fixed use case template.

--- src/use-cases/todo-item/create/create_todo_item.usecase.ts
+++ src/use-cases/todo-item/create/create_todo_item.usecase.ts
@@ -1,33 +1,32 @@
 import { TodoItemInMem } from "../../../entities/implementations/in-mem/todo_item_in_mem.entity";
-import { Repository } from "../../../repositories/abstractions/repository";
+import { RepoCreate } from "../../../repositories/abstractions/repo_create";
 import { TodoItemInMemRepository } from "../../../repositories/implementations/in-mem/todo_item_in_mem.repository";
-import { UseCase } from "../../abstractions/usecase";
 import { UseCaseTemplate } from "../../abstractions/usecase_template";
 import { Validator } from "../../abstractions/validator";
 import { CreateTodoItemInputDto } from "./dtos/create_todo_item_input.dto";
 import { CreateTodoItemOutputDto } from "./dtos/create_todo_item_output.dto";
 import { CreateTodoItemInputValidator } from "./validators/create_todo_item_input_validator";
 
-export class CreateTodoItemUseCase
-  extends UseCaseTemplate<CreateTodoItemInputDto, CreateTodoItemOutputDto>
-  implements UseCase<CreateTodoItemInputDto, CreateTodoItemOutputDto>
-{
+export class CreateTodoItemUseCase extends UseCaseTemplate<
+  CreateTodoItemInputDto,
+  CreateTodoItemOutputDto
+> {
   protected readonly _inputValidator: Validator<CreateTodoItemInputDto>;
-  private readonly _repo: Repository<TodoItemInMem>;
+  private readonly _repoCreate: RepoCreate<TodoItemInMem>;
 
   public constructor(
     inputValidator: Validator<CreateTodoItemInputDto> = new CreateTodoItemInputValidator(),
-    repo: Repository<TodoItemInMem> = new TodoItemInMemRepository()
+    repoCreate: RepoCreate<TodoItemInMem> = new TodoItemInMemRepository()
   ) {
     super();
     this._inputValidator = inputValidator;
-    this._repo = repo;
+    this._repoCreate = repoCreate;
   }
 
   public async handleLogic(
     input: CreateTodoItemInputDto
   ): Promise<CreateTodoItemOutputDto> {
-    const createdItem = await this._repo.create({ ...input });
+    const createdItem = await this._repoCreate.create({ ...input });
     const outputDto: CreateTodoItemOutputDto = { ...createdItem };
     return outputDto;
   }
