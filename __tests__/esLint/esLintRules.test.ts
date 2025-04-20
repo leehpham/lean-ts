f1ca4de23e833df7148bb6e734bdc3bad4592e69
Hoang Pham <lehoangpham1092@gmail.com>
2025-04-20 22:41:11 +0700

    feat: [LT-15] Added test case 'Missing semicolon'.

--- __tests__/esLint/esLintRules.test.ts
+++ __tests__/esLint/esLintRules.test.ts
@@ -1,14 +1,6 @@
 import { describe, expect, test } from "@jest/globals";
 import { ESLint } from "eslint";
 
-const makeEsLint = (): ESLint => new ESLint();
-
-const runLint = async (code: string): Promise<string | null> => {
-  const esLint = makeEsLint();
-  const lintResults = await esLint.lintText(code);
-  return lintResults[0].messages[0].ruleId;
-};
-
 const disableNoUnusedVarsCheck =
   "// eslint-disable-next-line @typescript-eslint/no-unused-vars";
 const disableEolLastCheck =
@@ -16,13 +8,26 @@ const disableEolLastCheck =
 
 describe("ESLint rules", () => {
   test("'Strings must use doublequote' reported, passed", async () => {
+    const esLint = new ESLint();
     const ruleId = "@stylistic/ts/quotes";
     const code = `
       ${disableNoUnusedVarsCheck}
       const foo = 'bar';
       ${disableEolLastCheck}
     `;
-    const reportedRuleId = await runLint(code);
-    expect(reportedRuleId).toBe(ruleId);
+    const lintResults = await esLint.lintText(code);
+    expect(lintResults[0].messages[0].ruleId).toBe(ruleId);
+  });
+
+  test("'Missing semicolon' reported, passed", async () => {
+    const esLint = new ESLint();
+    const ruleId = "@stylistic/ts/semi";
+    const code = `
+      ${disableNoUnusedVarsCheck}
+      const foo = 123
+      ${disableEolLastCheck}
+    `;
+    const lintResults = await esLint.lintText(code);
+    expect(lintResults[0].messages[0].ruleId).toBe(ruleId);
   });
 });
