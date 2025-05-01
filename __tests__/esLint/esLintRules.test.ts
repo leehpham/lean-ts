aba18db8d4fad890073b6f2c26511c45266df81d
Hoang Pham <lehoangpham1092@gmail.com>
2025-05-01 18:31:37 +0700

    feat: [LT-15] Added tests for @stylistic/ts/member-delimiter-style. Refactored tests for @stylistic/ts/semi. Refactored tests for @stylistic/ts/quotes.

--- __tests__/esLint/esLintRules.test.ts
+++ __tests__/esLint/esLintRules.test.ts
@@ -1,37 +1,83 @@
 import { describe, expect, test } from "@jest/globals";
 import { ESLint } from "eslint";
 
+const quotesRuleId = "@stylistic/ts/quotes";
+const semiRuleId = "@stylistic/ts/semi";
+const memberDelimiterStyleRuleId = "@stylistic/ts/member-delimiter-style";
+
 const disableNoUnusedVarsCheck =
   "// eslint-disable-next-line @typescript-eslint/no-unused-vars";
 const disableEolLastCheck =
   "// eslint-disable-next-line @stylistic/js/eol-last";
 
 describe("ESLint rules", () => {
-  test("'Strings must use doublequote' reported, passed", async () => {
-    const esLint = new ESLint();
-    const ruleId = "@stylistic/ts/quotes";
-    const code = `
+  describe(`${quotesRuleId}`, () => {
+    test("'Strings must use doublequote' reported, passed", async () => {
+      const esLint = new ESLint();
+      const code = `
       ${disableNoUnusedVarsCheck}
       const foo = 'bar';
       ${disableEolLastCheck}
     `;
-    const lintResults = await esLint.lintText(code);
-    expect(lintResults).toHaveLength(1);
-    expect(lintResults[0].messages).toHaveLength(1);
-    expect(lintResults[0].messages[0].ruleId).toBe(ruleId);
+      const lintResults = await esLint.lintText(code);
+      expect(lintResults).toHaveLength(1);
+      const onlyLintResult = lintResults[0];
+      expect(onlyLintResult.messages).toHaveLength(1);
+      const onlyMessage = onlyLintResult.messages[0];
+      expect(onlyMessage.ruleId).toBe(quotesRuleId);
+    });
   });
 
-  test("'Missing semicolon' reported, passed", async () => {
-    const esLint = new ESLint();
-    const ruleId = "@stylistic/ts/semi";
-    const code = `
+  describe(`${semiRuleId}`, () => {
+    test("'Missing semicolon' reported, passed", async () => {
+      const esLint = new ESLint();
+      const code = `
       ${disableNoUnusedVarsCheck}
       const foo = 123
       ${disableEolLastCheck}
     `;
-    const lintResults = await esLint.lintText(code);
-    expect(lintResults).toHaveLength(1);
-    expect(lintResults[0].messages).toHaveLength(1);
-    expect(lintResults[0].messages[0].ruleId).toBe(ruleId);
+      const lintResults = await esLint.lintText(code);
+      expect(lintResults).toHaveLength(1);
+      const onlyLintResult = lintResults[0];
+      expect(onlyLintResult.messages).toHaveLength(1);
+      const onlyMessage = onlyLintResult.messages[0];
+      expect(onlyMessage.ruleId).toBe(semiRuleId);
+    });
+  });
+
+  describe(`${memberDelimiterStyleRuleId}`, () => {
+    test("Line break used, 'Expected a semicolon' reported, passed", async () => {
+      const esLint = new ESLint();
+      const code = `
+      ${disableNoUnusedVarsCheck}
+      interface Foo {
+        bar: number
+      }
+      ${disableEolLastCheck}
+      `;
+      const lintResults = await esLint.lintText(code);
+      expect(lintResults).toHaveLength(1);
+      const onlyLintResult = lintResults[0];
+      expect(onlyLintResult.messages).toHaveLength(1);
+      const onlyMessage = onlyLintResult.messages[0];
+      expect(onlyMessage.ruleId).toBe(memberDelimiterStyleRuleId);
+    });
+
+    test("Comma used, 'Expected a semicolon' not reported, passed", async () => {
+      const esLint = new ESLint();
+      const code = `
+      ${disableNoUnusedVarsCheck}
+      interface Foo {
+        bar: number,
+      }
+      ${disableEolLastCheck}
+      `;
+      const lintResults = await esLint.lintText(code);
+      expect(lintResults).toHaveLength(1);
+      const onlyLintResult = lintResults[0];
+      expect(onlyLintResult.messages).toHaveLength(1);
+      const onlyMessage = onlyLintResult.messages[0];
+      expect(onlyMessage.ruleId).toBe(memberDelimiterStyleRuleId);
+    });
   });
 });
